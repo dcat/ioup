9266ef8db58ec2caa5ed8ead84271a2878971ae4
Broseph Tyrone Goldstein <dcat@iotek.org>
2014-06-27 21:48:04 +0200

    added readability

--- ioup.c
+++ ioup.c
@@ -2,7 +2,7 @@
  *      ioup.c - upload files to pub.iotek.org
  *  
  *
- *      Copyright (c) 2013, IOTek <deadcat (at) iotek (dot) org>
+ *      Copyright (c) 2014, Broseph <dcat (at) iotek (dot) org>
  *
  *      Permission to use, copy, modify, and/or distribute this software for any
  *      purpose with or without fee is hereby granted, provided that the above
@@ -20,23 +20,31 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <string.h>
+#include <stdbool.h>
 #include <sys/stat.h>
 #include <curl/curl.h>
+
+#define IOUP_BASE	"http://pub.iotek.org/"
+#define IOUP_LIST	IOUP_BASE "p/list.php"
+#define IOUP_REMOVE	IOUP_BASE "remove.php"
+#define IOUP_UPLOAD	IOUP_BASE "post.php"
 
 typedef struct {
 	const char *xt;
 	char *token,*name,*file;
 	bool list,remove,std_in;
-} io_t;
+} ioup_t;
 
-const char *get_xt (const char *s) {
+/* get string after last dot */
+const char *last_dot (const char *s) {
 	const char *d = strrchr(s, '.');
 	return ! d || d == s ? NULL : d + 1;
 }
 
-char *read_token () {
+/* read $HOME/.iouprc */
+#ifndef TOKEN
+char *read_iouprc (void) {
 	int i,c;
 	FILE *fp;
 	char *path,token[12],*r;
@@ -44,6 +52,7 @@ char *read_token () {
 	path = getenv("HOME");
 	strncat(path, "/.iouprc", 9);
 
+	/* byte by byte read */
 	if ((fp = fopen(path, "r"))) {
 		for (i=0; i < 10; i++) {
 			c = fgetc(fp);
@@ -57,13 +66,14 @@ char *read_token () {
 
 	return r;
 }
+#endif
 
-void io_post (io_t io) {
-	CURL            *c;
-	CURLcode        res;
-	char *url = io.list ? "http://pub.iotek.org/p/list.php" : "http://pub.iotek.org/post.php";
+void io_post (ioup_t io) {
 	FILE *in,*out;
+	char *url = io.list ? IOUP_LIST : IOUP_UPLOAD;
 	int chr;
+	CURL *c;
+	CURLcode res;
 
 	struct curl_httppost *formpost  = NULL;
 	struct curl_httppost *lastptr   = NULL;
@@ -71,15 +81,19 @@ void io_post (io_t io) {
 	struct stat sstat;
 	static const char buf[] = "Expect:";
 
+	/* read from stdin */
 	if (io.std_in) {
+		/* interactive mode? */
 		fstat(0, &sstat);
-		puts(S_ISCHR(sstat.st_mode) ? "^C: exit, ^D: post" : "");
+		if (S_ISCHR(sstat.st_mode)) 
+			puts("^C: exit, ^D: post");
 		
 		io.file = "/tmp/ioup.stdin";
 		io.name = "stdin";
 		out     = fopen(io.file, "w");
 		in      = stdin;
 
+		/* write to temp file */
 		if (in && out) {
 			while (!feof(in)) {
 				chr = fgetc(in);
@@ -90,7 +104,8 @@ void io_post (io_t io) {
 
 		fclose(in);
 		fclose(out);
-	} else io.xt = get_xt(io.file);
+	} else
+		io.xt = last_dot(io.file);
 
 	curl_global_init(CURL_GLOBAL_ALL);
 
@@ -121,7 +136,7 @@ void io_post (io_t io) {
 	}
 
 	if (io.remove) {
-		url = "http://pub.iotek.org/remove.php";
+		url = IOUP_REMOVE;
 		curl_formadd(	&formpost,
 				&lastptr,
 				CURLFORM_COPYNAME, io.file,
@@ -143,8 +158,10 @@ void io_post (io_t io) {
 		curl_easy_setopt(c, CURLOPT_HTTPHEADER, headerlist);
 		curl_easy_setopt(c, CURLOPT_HTTPPOST, formpost);
 		res = curl_easy_perform(c);
-		if (res != CURLE_OK) fprintf(stderr, "error: %s", curl_easy_strerror(res));
-		if (io.std_in) unlink(io.file);
+		if (res != CURLE_OK)
+			fprintf(stderr, "error: %s", curl_easy_strerror(res));
+		if (io.std_in)
+			unlink(io.file);
 		curl_easy_cleanup(c);
 		curl_formfree(formpost);
 		curl_slist_free_all(headerlist);
@@ -152,28 +169,32 @@ void io_post (io_t io) {
 }
 
 int main (int argc, char *argv[]) {
-	io_t io;
+	ioup_t io;
+	/* if TOKEN defined, hardcode it */
 	io.token =
 #ifdef TOKEN
 	TOKEN;
 #else
-	read_token();
+	read_iouprc();
 #endif
-	io.list  = io.remove = io.std_in = false;
-	io.file  = (argc >= 2) ? argv[1] : NULL;
-	io.name  = (argc >= 2) ? argv[1] : "stdin";
+	io.list = io.remove = io.std_in = false;
+	io.file = (argc >= 2) ? argv[1] : NULL;
+	io.name = (argc >= 2) ? argv[1] : "stdin";
 
 	if (argc >= 2 && argv[1][0] == '-') {
 		switch  (argv[1][1]) {
-			case 'l': io.list = true;
-				  break;
-			case 'r': if (argc > 1) {
-				printf("%s\n", argv[2]);
-				io.file = argv[2];
-				io.remove = true;
+			case 'l':
+				io.list = true;
+				break;
+			case 'r':
+				if (argc > 1) {
+					printf("%s\n", argv[2]);
+					io.file = argv[2];
+					io.remove = true;
 				}
 				break;
-			case 'V': printf("ioup-%s\n", VERSION);
+			case 'V':
+				printf("ioup-%s\n", VERSION);
 				return 0;
 		}
 	}
